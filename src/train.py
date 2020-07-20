69009244fbe89656ee1b05205d42aff0178d3c13
natepap <47045300+natepap@users.noreply.github.com>
2020-07-20 14:18:06 -0400

    - train.py now uses RandomForestRegressor instead of classifier (whoops)

--- src/train.py
+++ src/train.py
@@ -1,9 +1,5 @@
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import preprocessing
-from sklearn import metrics
-from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd
-import numpy as np
 import os
 import pickle
 
@@ -45,15 +41,14 @@ for output in outputHeaders:
 x = dataSet[inputHeaders].values
 y = dataSet[outputHeaders[0]].values
 
-lab_enc = preprocessing.LabelEncoder()
-encoded = lab_enc.fit_transform(y)
 
 
-model = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
+model = RandomForestRegressor(max_depth=2, random_state=0)
+model.fit(x,y)
 
-score = cross_val_score(model, x, encoded, cv=5)
+score = model.score(x,y)
 
-print(f'Cross val score = {score.mean()}')
+print(f'Score = {score}')
 
 with open(savePath + "network", "wb") as file:
     pickle.dump(model, file)
